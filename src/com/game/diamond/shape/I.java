642197500619749598d99f8aec7f19c1110dac50
hucheng <1759918385@qq.com>
2023-03-29 14:36:52 +0800

    代码首次提交

---
+++ src/com/game/diamond/shape/I.java
@@ -0,0 +1,27 @@
+package com.game.diamond.shape;
+
+import com.game.diamond.App.Tetris;
+import com.game.diamond.block.Cell;
+import com.game.diamond.block.Tetromino;
+
+/**
+ * @author xiaoZhao
+ * @date 2022/5/11
+ * @describe
+ */
+public class I extends Tetromino {
+
+    public I() {
+        cells[0] = new Cell(0,4, Tetris.I);
+        cells[1] = new Cell(0,3, Tetris.I);
+        cells[2] = new Cell(0,5, Tetris.I);
+        cells[3] = new Cell(0,6, Tetris.I);
+
+        //共有两种旋转状态
+        states =new State[2];
+        //初始化两种状态的相对坐标
+        states[0]=new State(0,0,0,-1,0,1,0,2);
+        states[1]=new State(0,0,-1,0,1,0,2,0);
+    }
+
+}
---
+++ src/com/game/diamond/shape/T.java
@@ -0,0 +1,25 @@
+package com.game.diamond.shape;
+
+import com.game.diamond.App.Tetris;
+import com.game.diamond.block.Cell;
+import com.game.diamond.block.Tetromino;
+
+/**
+ * @author xiaoZhao
+ * @date 2022/5/11
+ * @describe
+ */
+public class T extends Tetromino {
+    public T() {
+        cells[0] = new Cell(0,4, Tetris.T);
+        cells[1] = new Cell(0,3, Tetris.T);
+        cells[2] = new Cell(0,5, Tetris.T);
+        cells[3] = new Cell(1,4, Tetris.T);
+
+        states=new State[4];
+        states[0]=new State(0,0,0,-1,0,1,1,0);
+        states[1]=new State(0,0,-1,0,1,0,0,-1);
+        states[2]=new State(0,0,0,1,0,-1,-1,0);
+        states[3]=new State(0,0,1,0,-1,0,0,1);
+    }
+}
